2121a4403ad5c3767863dd1d5d0879979c8ce559
Dice <monoxlin@outlook.com>
2024-06-18 14:53:36 +0300

    v0.2 - v0.3

--- Content/GUI/PotionSlotGui.cs
+++ Content/GUI/PotionSlotGui.cs
@@ -2,179 +2,206 @@ using PotionSlots.Core.Loaders.UILoading;
 using ReLogic.Graphics;
 using System;
 using System.Collections.Generic;
+using Terraria;
 using Terraria.Audio;
 using Terraria.ID;
 using Terraria.UI;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.GameContent.UI.Elements;
 
 namespace PotionSlots.Content.GUI
 {
-	public class PotionSlotGui : SmartUIState
-	{
-		LifeSlot life = new();
-		ManaSlot mana = new();
+    public class PotionSlotGui : SmartUIState
+    {
+        private LifeSlot life;
+        private ManaSlot mana;
+        private WormholeSlot wormhole;
 
-		public override bool Visible => Main.playerInventory;
+        public override bool Visible => Main.playerInventory;
 
-		public override int InsertionIndex(List<GameInterfaceLayer> layers)
-		{
-			return layers.FindIndex(layer => layer.Name.Equals("Vanilla: Mouse Text"));
-		}
+        public override int InsertionIndex(List<GameInterfaceLayer> layers)
+        {
+            return layers.FindIndex(layer => layer.Name.Equals("Vanilla: Mouse Text"));
+        }
 
-		public override void OnInitialize()
-		{
-			AddElement(life, 572, 107, 28, 28);
-			AddElement(mana, 572, 139, 28, 28);
-		}
+        public override void OnInitialize()
+        {
+            life = new LifeSlot();
+            mana = new ManaSlot();
+            wormhole = new WormholeSlot();
 
-		public override void SafeUpdate(GameTime gameTime)
-		{
-			// Debug
-			if (Main.LocalPlayer.controlHook)
-			{
-				RemoveAllChildren();
-				OnInitialize();
-			}
-		}
+            SetSlotProperties(life, 571, 105);
+            SetSlotProperties(mana, 571, 138);
+            SetSlotProperties(wormhole, 571, 172);
 
-		public override void Draw(SpriteBatch spriteBatch)
-		{
-			var font = Terraria.GameContent.FontAssets.MouseText.Value;
-			spriteBatch.DrawString(font, "Potions", new Vector2(570, 85), Main.MouseTextColorReal, 0f, Vector2.Zero, 0.7f, 0, 0);
+            Append(life);
+            Append(mana);
+            Append(wormhole);
+        }
 
-			base.Draw(spriteBatch);
-		}
-	}
+        private void SetSlotProperties(UIElement slot, float left, float top)
+        {
+            slot.Width.Set(31, 0);
+            slot.Height.Set(31, 0);
+            slot.Left.Set(left, 0);
+            slot.Top.Set(top, 0);
+        }
 
-	public abstract class PotionSlot : SmartUIElement
-	{
-		public abstract ref Item item { get; }
+        public override void SafeUpdate(GameTime gameTime)
+        {
+            // Debug: Re-initialize when controlHook is pressed
+            if (Main.LocalPlayer.controlHook)
+            {
+                RemoveAllChildren();
+                OnInitialize();
+            }
+        }
 
-		public abstract Func<Item, bool> isValid { get; }
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            var font = Terraria.GameContent.FontAssets.MouseText.Value;
+            spriteBatch.DrawString(font, "Potions", new Vector2(570, 85), Main.MouseTextColorReal, 0f, Vector2.Zero, 0.7f, 0, 0);
 
-		public abstract string Texture { get; }
+            base.Draw(spriteBatch);
+        }
+    }
 
-		public abstract string TextureFilled { get; }
+    public abstract class PotionSlot : SmartUIElement
+    {
+        public abstract ref Item item { get; }
 
-		public bool NoItem => item is null || item.IsAir;
+        public abstract Func<Item, bool> isValid { get; }
 
+        public abstract string Texture { get; }
 
-		public PotionSlot()
-		{
-			// TOOD: Adjust for texture
-			Width.Set(42, 0);
-			Height.Set(42, 0);
-		}
+        public abstract string TextureFilled { get; }
 
-		public override void SafeClick(UIMouseEvent evt)
-		{
-			if (!Main.mouseItem.IsAir && isValid(Main.mouseItem) && NoItem)
-			{
-				item = Main.mouseItem.Clone();
+        public bool NoItem => item is null || item.IsAir;
 
-				Main.LocalPlayer.HeldItem.TurnToAir();
-				Main.mouseItem.TurnToAir();
-				SoundEngine.PlaySound(SoundID.Grab);
-			}
-			else if (Main.mouseItem.IsAir && !NoItem)
-			{
-				Main.mouseItem = item.Clone();
-				item.TurnToAir();
-				SoundEngine.PlaySound(SoundID.Grab);
-			}
-			else if (!Main.mouseItem.IsAir && isValid(Main.mouseItem) && !NoItem)
-			{
-				if (Main.mouseItem.type != item.type)
-				{
-					var temp = item;
-					item = Main.mouseItem.Clone();
-					Main.mouseItem = temp.Clone();
-				}
-				else
-				{
-					var joined = item.stack + Main.mouseItem.stack;
-					if (joined > item.maxStack)
-					{
-						item.stack = item.maxStack;
-						Main.mouseItem.stack = joined - item.maxStack;
-					}
-					else
-					{
-						item.stack = joined;
-						Main.mouseItem.TurnToAir();
-					}
-				}
-				SoundEngine.PlaySound(SoundID.Grab);
-			}
-		}
+        public PotionSlot()
+        {
+            Width.Set(42, 0);
+            Height.Set(42, 0);
+        }
 
-		public override void SafeRightClick(UIMouseEvent evt)
-		{
-			if (Main.mouseItem.IsAir && !NoItem)
-			{
-				var temp = item.Clone();
-				temp.stack = 1;
-				Main.mouseItem = temp;
+        public override void SafeClick(UIMouseEvent evt)
+        {
+            if (!Main.mouseItem.IsAir && isValid(Main.mouseItem) && NoItem)
+            {
+                item = Main.mouseItem.Clone();
+                Main.mouseItem.TurnToAir();
+                SoundEngine.PlaySound(SoundID.Grab);
+            }
+            else if (Main.mouseItem.IsAir && !NoItem)
+            {
+                Main.mouseItem = item.Clone();
+                item.TurnToAir();
+                SoundEngine.PlaySound(SoundID.Grab);
+            }
+            else if (!Main.mouseItem.IsAir && isValid(Main.mouseItem) && !NoItem)
+            {
+                if (Main.mouseItem.type != item.type)
+                {
+                    var temp = item.Clone();
+                    item = Main.mouseItem.Clone();
+                    Main.mouseItem = temp;
+                }
+                else
+                {
+                    var joined = item.stack + Main.mouseItem.stack;
+                    if (joined > item.maxStack)
+                    {
+                        item.stack = item.maxStack;
+                        Main.mouseItem.stack = joined - item.maxStack;
+                    }
+                    else
+                    {
+                        item.stack = joined;
+                        Main.mouseItem.TurnToAir();
+                    }
+                }
+                SoundEngine.PlaySound(SoundID.Grab);
+            }
+        }
 
-				item.stack--;
+        public override void SafeRightClick(UIMouseEvent evt)
+        {
+            if (Main.mouseItem.IsAir && !NoItem)
+            {
+                var temp = item.Clone();
+                temp.stack = 1;
+                Main.mouseItem = temp;
 
-				if (item.stack <= 0)
-					item.TurnToAir();
+                item.stack--;
 
-				SoundEngine.PlaySound(SoundID.MenuTick);
-			}
-			else if (!Main.mouseItem.IsAir && !NoItem && Main.mouseItem.type == item.type && Main.mouseItem.stack < Main.mouseItem.maxStack)
-			{
-				Main.mouseItem.stack++;
-				item.stack--;
+                if (item.stack <= 0)
+                    item.TurnToAir();
 
-				if (item.stack <= 0)
-					item.TurnToAir();
+                SoundEngine.PlaySound(SoundID.MenuTick);
+            }
+            else if (!Main.mouseItem.IsAir && !NoItem && Main.mouseItem.type == item.type && Main.mouseItem.stack < Main.mouseItem.maxStack)
+            {
+                Main.mouseItem.stack++;
+                item.stack--;
 
-				SoundEngine.PlaySound(SoundID.MenuTick);
-			}
-		}
+                if (item.stack <= 0)
+                    item.TurnToAir();
 
-		public override void Draw(SpriteBatch spriteBatch)
-		{
-			var tex = ModContent.Request<Texture2D>(NoItem ? Texture : TextureFilled).Value;
-			spriteBatch.Draw(tex, GetDimensions().ToRectangle(), Color.White);
+                SoundEngine.PlaySound(SoundID.MenuTick);
+            }
+        }
 
-			if (!NoItem)
-			{
-				Main.inventoryScale = 36 / 52f * 28 / 36f;
-				ItemSlot.Draw(spriteBatch, ref item, 21, GetDimensions().Position());
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            var tex = ModContent.Request<Texture2D>(NoItem ? Texture : TextureFilled).Value;
+            spriteBatch.Draw(tex, GetDimensions().ToRectangle(), Color.White);
 
-				if (IsMouseHovering)
-				{
-					Main.LocalPlayer.mouseInterface = true;
-					Main.HoverItem = item.Clone();
-					Main.hoverItemName = "a";
-				}
-			}
+            if (!NoItem)
+            {
+                Main.inventoryScale = 36 / 52f * 31 / 36f;
+                ItemSlot.Draw(spriteBatch, ref item, 21, GetDimensions().Position());
 
-			if (IsMouseHovering)
-				Main.LocalPlayer.mouseInterface = true;
-		}
-	}
+                if (IsMouseHovering)
+                {
+                    Main.LocalPlayer.mouseInterface = true;
+                    Main.HoverItem = item.Clone();
+                    Main.hoverItemName = "a";
+                }
+            }
 
-	public class LifeSlot : PotionSlot
-	{
-		public override ref Item item => ref Main.LocalPlayer.GetModPlayer<PotionStoragePlayer>().lifeSlot;
+            if (IsMouseHovering)
+                Main.LocalPlayer.mouseInterface = true;
+        }
+    }
 
-		public override Func<Item, bool> isValid => (item) => item.healLife > 0;
+    public class LifeSlot : PotionSlot
+    {
+        public override ref Item item => ref Main.LocalPlayer.GetModPlayer<PotionStoragePlayer>().lifeSlot;
 
-		public override string Texture => "PotionSlots/Assets/healing_sprite";
-		public override string TextureFilled => "PotionSlots/Assets/healingbg";
+        public override Func<Item, bool> isValid => (item) => item.healLife > 0;
 
-	}
+        public override string Texture => "PotionSlots/Assets/healing_sprite";
+        public override string TextureFilled => "PotionSlots/Assets/healingbg";
+    }
 
-	public class ManaSlot : PotionSlot
-	{
-		public override ref Item item => ref Main.LocalPlayer.GetModPlayer<PotionStoragePlayer>().manaSlot;
+    public class ManaSlot : PotionSlot
+    {
+        public override ref Item item => ref Main.LocalPlayer.GetModPlayer<PotionStoragePlayer>().manaSlot;
 
-		public override Func<Item, bool> isValid => (item) => item.healMana > 0;
+        public override Func<Item, bool> isValid => (item) => item.healMana > 0;
 
-		public override string Texture => "PotionSlots/Assets/mana_sprite";
-		public override string TextureFilled => "PotionSlots/Assets/manabg";
-	}
+        public override string Texture => "PotionSlots/Assets/mana_sprite";
+        public override string TextureFilled => "PotionSlots/Assets/manabg";
+    }
+
+    public class WormholeSlot : PotionSlot
+    {
+        public override ref Item item => ref Main.LocalPlayer.GetModPlayer<PotionStoragePlayer>().wormholeSlot;
+
+        public override Func<Item, bool> isValid => (item) => item.type == ItemID.WormholePotion;
+
+        public override string Texture => "PotionSlots/Assets/wormhole_sprite";
+        public override string TextureFilled => "PotionSlots/Assets/wormholebg";
+    }
 }
--- Core/PotionStoragePlayer.cs
+++ Core/PotionStoragePlayer.cs
@@ -1,51 +1,143 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
 using Terraria.ModLoader.IO;
+using Terraria.ID;
+using Terraria.Audio;
 
 namespace PotionSlots.Core
 {
-	internal class PotionStoragePlayer : ModPlayer
-	{
-		public Item lifeSlot;
-		public Item manaSlot;
+    internal class PotionStoragePlayer : ModPlayer
+    {
+        public Item lifeSlot;
+        public Item manaSlot;
+        public Item wormholeSlot;
 
-		public override void Load()
-		{
-			On_Player.QuickHeal_GetItemToUse += PickLifeSlot;
-			On_Player.QuickMana_GetItemToUse += PickManaSlot;
-		}
+        public override void Initialize()
+        {
+            lifeSlot = new Item();
+            manaSlot = new Item();
+            wormholeSlot = new Item();
+        }
 
-		private Item PickLifeSlot(On_Player.orig_QuickHeal_GetItemToUse orig, Player self)
-		{
-			var lifeSlot = self.GetModPlayer<PotionStoragePlayer>().lifeSlot;
-			if (lifeSlot != null && !lifeSlot.IsAir)
-				return lifeSlot;
+        public override void Load()
+        {
+            On_Player.QuickHeal_GetItemToUse += PickLifeSlot;
+            On_Player.QuickMana_GetItemToUse += PickManaSlot;
+        }
 
-			return orig(self);
-		}
+        private Item PickLifeSlot(On_Player.orig_QuickHeal_GetItemToUse orig, Player self)
+        {
+            var lifeSlot = self.GetModPlayer<PotionStoragePlayer>().lifeSlot;
+            if (lifeSlot != null && !lifeSlot.IsAir)
+                return lifeSlot;
 
-		private Item PickManaSlot(On_Player.orig_QuickMana_GetItemToUse orig, Player self)
-		{
-			var manaSlot = self.GetModPlayer<PotionStoragePlayer>().manaSlot;
-			if (manaSlot != null && !manaSlot.IsAir)
-				return manaSlot;
+            return orig(self);
+        }
 
-			return orig(self);
-		}
+        private Item PickManaSlot(On_Player.orig_QuickMana_GetItemToUse orig, Player self)
+        {
+            var manaSlot = self.GetModPlayer<PotionStoragePlayer>().manaSlot;
+            if (manaSlot != null && !manaSlot.IsAir)
+                return manaSlot;
 
-		public override void SaveData(TagCompound tag)
-		{
-			tag.Add("life", lifeSlot);
-			tag.Add("mana", manaSlot);
-		}
+            return orig(self);
+        }
 
-		public override void LoadData(TagCompound tag)
-		{
-			lifeSlot = tag.Get<Item>("life");
-			manaSlot = tag.Get<Item>("mana");
-		}
-	}
+        public override void SaveData(TagCompound tag)
+        {
+            tag.Add("life", lifeSlot);
+            tag.Add("mana", manaSlot);
+            tag.Add("wormhole", wormholeSlot);
+        }
+
+        public override void LoadData(TagCompound tag)
+        {
+            lifeSlot = tag.Get<Item>("life");
+            manaSlot = tag.Get<Item>("mana");
+            wormholeSlot = tag.Get<Item>("wormhole");
+        }
+
+        public override bool OnPickup(Item item)
+        {
+            bool pickedUp = false;
+
+            if (item.healLife > 0)
+            {
+                if (lifeSlot.IsAir || (lifeSlot.type == item.type && lifeSlot.stack < lifeSlot.maxStack))
+                {
+                    if (lifeSlot.IsAir)
+                    {
+                        lifeSlot = item.Clone();
+                        lifeSlot.stack = 0;
+                    }
+
+                    int transferableAmount = Math.Min(item.stack, lifeSlot.maxStack - lifeSlot.stack);
+                    lifeSlot.stack += transferableAmount;
+                    item.stack -= transferableAmount;
+                    pickedUp = true;
+
+                    if (item.stack <= 0)
+                    {
+                        item.TurnToAir();
+                        SoundEngine.PlaySound(SoundID.Grab);
+                        return false;
+                    }
+                }
+            }
+            else if (item.healMana > 0)
+            {
+                if (manaSlot.IsAir || (manaSlot.type == item.type && manaSlot.stack < manaSlot.maxStack))
+                {
+                    if (manaSlot.IsAir)
+                    {
+                        manaSlot = item.Clone();
+                        manaSlot.stack = 0;
+                    }
+
+                    int transferableAmount = Math.Min(item.stack, manaSlot.maxStack - manaSlot.stack);
+                    manaSlot.stack += transferableAmount;
+                    item.stack -= transferableAmount;
+                    pickedUp = true;
+
+                    if (item.stack <= 0)
+                    {
+                        item.TurnToAir();
+                        SoundEngine.PlaySound(SoundID.Grab);
+                        return false;
+                    }
+                }
+            }
+            else if (item.type == ItemID.WormholePotion)
+            {
+                if (wormholeSlot.IsAir || (wormholeSlot.type == item.type && wormholeSlot.stack < wormholeSlot.maxStack))
+                {
+                    if (wormholeSlot.IsAir)
+                    {
+                        wormholeSlot = item.Clone();
+                        wormholeSlot.stack = 0;
+                    }
+
+                    int transferableAmount = Math.Min(item.stack, wormholeSlot.maxStack - wormholeSlot.stack);
+                    wormholeSlot.stack += transferableAmount;
+                    item.stack -= transferableAmount;
+                    pickedUp = true;
+
+                    if (item.stack <= 0)
+                    {
+                        item.TurnToAir();
+                        SoundEngine.PlaySound(SoundID.Grab);
+                        return false;
+                    }
+                }
+            }
+
+            if (pickedUp)
+            {
+                SoundEngine.PlaySound(SoundID.Grab);
+            }
+
+            return true;
+        }
+    }
 }
